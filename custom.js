c020850b34d951afde6d44fa93e42db455a057cd
zfdesign <zlatinivanov01@gmail.com>
2013-07-21 23:51:36 +0100

    Added Page background scroll effect

--- custom.js
+++ custom.js
@@ -1,18 +1,12 @@
-window._t = window._t || {};
-_t.hasjQuery = !!window.jQuery;
-
-// scrollClass
-_t.scrollClass = function (data) {
-    console.log(data);
+var toggleBodyClass = function (scrlPos) {
+    if (document.body.className.indexOf('scroll') !== -1) {
+        document.body.className = document.body.className.replace(/\s?(scroll)[0-9]*/i, "");
+    }
+    scrlPos = (scrlPos > 900) ? 900 : scrlPos;
+    document.body.className += " scroll" + scrlPos;
 };
+window.onscroll = function (e) {
+    //console.log(window.scrollY);
+    toggleBodyClass(window.scrollY);
+}
 
-
-if (_t.hasjQuery) {
-    jQuery(document).ready(function () {
-        jQuery(window).scroll(function (e) {
-            _t.scrollClass(e.target);
-        });
-    });
-
-
-} // _t.hasjQuery

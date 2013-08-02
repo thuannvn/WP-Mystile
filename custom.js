250842883adecfc125c79257a5a33bdb55c59129
zfdesign <zlatinivanov01@gmail.com>
2013-08-02 16:36:45 +0300

    Product list style update. JavaScript scroll fix and transitions

--- custom.js
+++ custom.js
@@ -2,6 +2,8 @@ var toggleBodyClass = function (scrlPos) {
     if (document.body.className.indexOf('scroll') !== -1) {
         document.body.className = document.body.className.replace(/\s?(scroll)[0-9]*/i, "");
     }
+
+    scrlPos = (scrlPos < 100 && scrlPos !== 0) ? 100 : scrlPos; // Mac OS fix 
     scrlPos = (scrlPos > 900) ? 900 : scrlPos;
     document.body.className += " scroll" + scrlPos;
 };
